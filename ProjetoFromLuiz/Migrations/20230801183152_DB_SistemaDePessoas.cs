6d25f8b4737f14fd8c33ecb3c683698d0737140d
pessoamarcio <pessoa-marcio@hotmail.com>
2023-08-08 09:35:36 -0300

    nova configuração no sql

---
+++ ProjetoFromLuiz/Migrations/20230801183152_DB_SistemaDePessoas.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoFromLuiz.Migrations
+{
+    public partial class DB_SistemaDePessoas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
